c3b3dae42139534c38d75b2023d9fb2490cb94b1
Manuel Sanchez <livediang@gmail.com>
2026-06-12 14:10:01 -0500

    Project: US-003 Build Utility CRUD Angular - Services

--- code_frontend/CRUD_Angular22_App/src/app/Features/ObjBooks/Services/books-service.ts
+++ code_frontend/CRUD_Angular22_App/src/app/Features/ObjBooks/Services/books-service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { GetBookResponse } from '../../../Shared/Interfaces/get-book-response';
+import { CreateBookRequest } from '../../../Shared/Interfaces/create-book-request';
+import { UpdateBookRequest } from '../../../Shared/Interfaces/update-book-request';
 
 @Injectable({
   providedIn: 'root',
 })
-export class BooksService {}
+export class BooksService {
+  private http = inject(HttpClient)
+  private endPoint = `${environment}/books`
+
+  getAll():Observable<GetBookResponse[]>{
+    return this.http.get<GetBookResponse[]>(this.endPoint);
+  }
+
+  getById(id:number):Observable<GetBookResponse>{
+    return this.http.get<GetBookResponse>(`${this.endPoint}/${id}`);
+  }
+
+  create(request:CreateBookRequest):Observable<void>{
+    return this.http.post<void>(this.endPoint,request);
+  }
+
+  update(request:UpdateBookRequest):Observable<void>{
+    return this.http.put<void>(this.endPoint,request);
+  }
+
+  delete(id:number):Observable<void>{
+    return this.http.get<void>(`${this.endPoint}/${id}`);
+  }
+}
--- code_frontend/CRUD_Angular22_App/src/app/Features/ObjGenres/Services/genres-service.ts
+++ code_frontend/CRUD_Angular22_App/src/app/Features/ObjGenres/Services/genres-service.ts
@@ -1,6 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { GetGenreResponse } from '../../../Shared/Interfaces/get-genre-response';
 
 @Injectable({
   providedIn: 'root',
 })
-export class GenresService {}
+export class GenresService {
+  private http = inject(HttpClient)
+  private endPoint = `${environment}/books`
+
+  getAll():Observable<GetGenreResponse[]>{
+    return this.http.get<GetGenreResponse[]>(this.endPoint);
+  }
+}
